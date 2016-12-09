50541fb674f08fb7cf01e384a1cded672586f153
carico.jon <carico.jon@gmail.com>
2016-12-09 14:47:05 -0500

    Initial commit.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s-light-light-light
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s-light-light' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s-light-light' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s-light-light' ), '_s-light-light', '<a href="https://joncarico.github.io" rel="designer">Jon Carico</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
